3899aec6f80767b43b369e5e97f73286a1a8e54e
Zal <101911964+silentalex1@users.noreply.github.com>
2026-01-30 18:12:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require('http');
+const fs = require('fs');
 const path = require('path');
 
-app.use(express.json());
-app.use(express.static(__dirname));
+let latestMsg = "Awaiting Data...";
 
-app.post('/sync', (req, res) => {
-    io.emit('update', req.body.msg);
-    res.status(200).json({ success: true });
+const server = http.createServer((req, res) => {
+    if (req.url === '/sync') {
+        if (req.method === 'POST') {
+            let body = '';
+            req.on('data', chunk => { body += chunk.toString(); });
+            req.on('end', () => {
+                try {
+                    const data = JSON.parse(body);
+                    latestMsg = data.msg;
+                    res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+                    res.end(JSON.stringify({ ok: true }));
+                } catch (e) {
+                    res.writeHead(400);
+                    res.end();
+                }
+            });
+        } else if (req.method === 'GET') {
+            res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+            res.end(JSON.stringify({ msg: latestMsg }));
+        }
+    } else {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    }
 });
 
-app.get('/sync', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-http.listen(80, () => {
-    console.log('Server active');
-});
+server.listen();
