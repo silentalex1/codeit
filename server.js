eb4316fbb81fdce0bfeb4ae462b45b0a0546371f
Zal <101911964+silentalex1@users.noreply.github.com>
2026-05-28 21:01:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -57,6 +57,4 @@ app.get('/poll', (req, res) => {
 });
 
 const PORT = process.env.PORT || 8081;
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
+app.listen(PORT, () => {});
